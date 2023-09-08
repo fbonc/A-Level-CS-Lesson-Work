87f7cac70b9585bacdc5e653482be9e34960326e
felip <felipe.bonchristiano@outlook.com>
2023-09-08 10:23:26 +0100

    test 2

--- Multiplication table.py
+++ Multiplication table.py
@@ -10,12 +10,6 @@ def calculatetable(table):
 calculatetable(table)
 
 
-
-
-
-
-
-
 # table = int(input("What multiplication table would you like to view?"))
 # for i in range(1,13):
 #     print(f"{table} x {i} = {table*i}")
