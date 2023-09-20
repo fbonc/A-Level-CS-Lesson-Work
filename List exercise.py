ce74a9b6c4ae1dff1e1e6a32227bc1872c720d13
felip <felipe.bonchristiano@outlook.com>
2023-09-20 19:57:10 +0100

    List exercise updated

--- List exercise.py
+++ List exercise.py
@@ -1,21 +1,26 @@
-# name_list = ['Ed', 'William', 'Toby', 'Freddie', 'Rohan', 'Ian', 'Matthew', 'Gavin', 'Lenny', 'Thomas', 'Jake']
+def listappendingandindexpractice():
+    name_list = ['Ed', 'William', 'Toby', 'Freddie', 'Rohan', 'Ian', 'Matthew', 'Gavin', 'Lenny', 'Thomas', 'Jake']
 
-# for i in range(3):
-#     name = input("Type in a name: ")
-#     name_list.append(name)
+    for i in range(3):
+        name = input("Type in a name: ")
+        name_list.append(name)
 
-# print(name_list)
-# print(f"The third name is {name_list[2]}")
-# print(f"The last seven names are {name_list[-7:]}")
+    print(name_list)
+    print(f"The third name is {name_list[2]}")
+    print(f"The last seven names are {name_list[-7:]}")
 
-numbers = []
-for i in range(5):
-    number = int(input("Type in a number: "))
-    numbers.append(number)
 
-print(max(numbers))
-print(min(numbers))
-print(sum(numbers))
-print(sum(numbers)/len(numbers))
+def listnumberspractice():
+    numbers = []
+    for i in range(5):
+        number = int(input("Type in a number: "))
+        numbers.append(number)
+
+    print(max(numbers))
+    print(min(numbers))
+    print(sum(numbers))
+    print(sum(numbers)/len(numbers))
+
+listnumberspractice()
 
 # Filipe, good stuff. Rather than commenting out portions like this, could you write them in separate functions and then call only call the function you want.
