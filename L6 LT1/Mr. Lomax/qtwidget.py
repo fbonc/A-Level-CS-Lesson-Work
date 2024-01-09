ced176d95924cbdd27226e841bfc0232617e62b1
felip <felipe.bonchristiano@outlook.com>
2024-01-09 16:09:15 +0000

    qtwidget

---
+++ L6 LT1/Mr. Lomax/qtwidget.py
@@ -0,0 +1,57 @@
+import PyQt6.QtWidgets as qtw
+import PyQt6.QtCore as qtc
+# import PyQt6.QtGui as qtg
+
+class MainWindow(qtw.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My Awesome App")
+        self.setFixedSize(200,300)
+
+        layout = qtw.QVBoxLayout()
+        self.label = qtw.QLabel("Widget Demo")
+
+        self.comboBox = qtw.QComboBox()
+        self.comboBox.addItem("One")
+        self.comboBox.addItem("Two")
+        self.comboBox.addItem("Three")
+        self.comboBox.addItem("Four")
+        
+        self.checkBox1 = qtw.QCheckBox("Choose this")
+        self.checkBox2 = qtw.QCheckBox("and this?")
+
+        self.lineEdit = qtw.QLineEdit("Type here")
+        
+        self.RadioButton1 = qtw.QRadioButton("This one?")
+        self.RadioButton2 = qtw.QRadioButton("Or this one?")
+        
+        self.slider = qtw.QSlider(qtc.Qt.Orientation.Horizontal)
+        
+        self.button1 = qtw.QPushButton("Ok")
+        self.button2 = qtw.QPushButton("Cancel")
+
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.comboBox)
+        layout.addWidget(self.checkBox1)
+        layout.addWidget(self.checkBox2)
+        layout.addWidget(self.lineEdit)
+        layout.addWidget(self.RadioButton1)
+        layout.addWidget(self.RadioButton2)
+        layout.addWidget(self.slider)
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+
+        widget = qtw.QWidget()
+        widget.setLayout(layout)
+        
+        self.setCentralWidget(widget)
+
+
+app = qtw.QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
