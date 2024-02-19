d55f684e5c277c89f5a10e7d66fc64f65860d5db
felip <felipe.bonchristiano@outlook.com>
2024-02-19 20:23:17 +0000

    flaskdiscussionboard

--- L6 LT1/Mr. Lomax/flask/flaskintro.py
+++ L6 LT1/Mr. Lomax/flask/flaskintro.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template, request
-from flask import request
 import datetime
 
 app = Flask(__name__)
 
-
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 @app.route('/add')
 def add(): 
