2b758e140e8b7e2accb41129ff7bfdcb482d3442
felip <felipe.bonchristiano@outlook.com>
2024-02-21 00:34:17 +0000

    finished flaskdicussionboard app

--- L6 LT1/Mr. Lomax/flaskdiscussionboard/main.py
+++ L6 LT1/Mr. Lomax/flaskdiscussionboard/main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import datetime
 
 app = Flask(__name__)
+app.secret_key = '1234567890'
 
-@app.route('/')
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    filename = r'L6 LT1\Mr. Lomax\flaskdiscussionboard\comments.txt'
+    with open(filename, 'r') as file:
+        text = file.read()
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        session['username'] = username
+        comment = request.form.get('comment')
+
+        if comment:
+            now = datetime.datetime.now()
+            current_time = now.strftime("%H:%M")
+            today = datetime.date.today()
+
+            with open(filename, 'a') as file:
+                entry = f"[{current_time}, {today}] {session.get('username', 'Anon')}--:::::--{comment}\n"
+                file.write(entry)
+        
+
+    with open(filename, 'r') as file:
+        text = file.read()
+
+        return render_template('index.html', text=text, username=session.get('username', ''))
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=80, debug=True)
